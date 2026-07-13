4ab941cef6144a84668ce5c8d6c9776abba2ccb5
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2026-07-13 21:46:58 +0300

    feat(auth): Create authorization code.

--- auth.ts
+++ auth.ts
@@ -1,5 +1,33 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize(credentials) {
+        const username = credentials?.username;
+        const password = credentials?.password;
+
+        if (
+          username === process.env.ADMIN_USERNAME &&
+          password === process.env.ADMIN_PASSWORD
+        ) {
+          return {
+            id: "1",
+            name: "Admin",
+            email: "admin@eshop.com",
+          };
+        }
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
 });
