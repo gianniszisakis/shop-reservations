afcf722c9bed80e288dba97a1e262cb4dbec886a
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2026-08-15 18:42:46 +0300

    feat: Replace static data of header with api data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+"use client";
 import Header from "@/components/header/header";
 import { LatestBookingsSection } from "@/components/latest-bookings/latest-bookings-section";
+import { useAppointments } from "@/features/appointments/queries";
+import { useMemo } from "react";
 
 export default function Home() {
+  const { data: appointments, isLoading, isError } = useAppointments();
+
+  const { activeAppointments, todayAppointments } = useMemo(() => {
+    const today = new Date().toISOString().split("T")[0];
+
+    const confirmedAppointments =
+      appointments?.filter(
+        (appointment) => appointment.status === "CONFIRMED",
+      ) ?? [];
+
+    return {
+      activeAppointments: confirmedAppointments.filter(
+        (appointment) => appointment.startDateTime.split("T")[0] >= today,
+      ),
+
+      todayAppointments: confirmedAppointments.filter(
+        (appointment) => appointment.startDateTime.split("T")[0] === today,
+      ),
+    };
+  }, [appointments]);
+
   return (
     <>
       <Header
@@ -11,8 +35,10 @@ export default function Home() {
         avatarAlt="After Glow Logo"
         calendarColor="text-pink-600"
         infoTextboxBgColor="bg-pink-50"
-        textInfoOne="128 ενεργά ραντεβού"
-        textInfoTwo="Σήμερα: 12 ραντεβού"
+        textInfoOne={`${activeAppointments?.length} ενεργά ραντεβού`}
+        textInfoTwo={`Σήμερα: ${todayAppointments?.length} ραντεβού`}
+        isLoading={isLoading}
+        isError={isError}
       />
       <div className="mx-5 sm:mx-10">
         <LatestBookingsSection />
