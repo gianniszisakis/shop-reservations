52e645b47a4cfdbc80585cddd5fc34a9913af586
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2026-09-01 16:28:24 +0300

    feat: Change button behavior when page is in appointments.

--- src/components/header/historic-data-button.tsx
+++ src/components/header/historic-data-button.tsx
@@ -1,21 +1,33 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { HistoryIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { HistoryIcon, Home } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 
 export function HistoricDataButton() {
   const router = useRouter();
+  const pathname = usePathname();
+
+  const isSettingsPage = pathname.startsWith("/appointments");
 
   return (
     <Button
       type="button"
       size="lg"
       className="gap-2 rounded-xl bg-pink-600 px-6 transition-shadow duration-200 hover:bg-pink-500 hover:shadow-lg"
-      onClick={() => router.push("/appointments")}
+      onClick={() => router.push(isSettingsPage ? "/" : "/appointments")}
     >
-      <HistoryIcon className="h-5 w-5" />
-      Ιστορικό
+      {isSettingsPage ? (
+        <>
+          <Home className="h-5 w-5" />
+          Αρχική
+        </>
+      ) : (
+        <>
+          <HistoryIcon className="h-5 w-5" />
+          Ιστορικό Ραντεβού
+        </>
+      )}
     </Button>
   );
 }
