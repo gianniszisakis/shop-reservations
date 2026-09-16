91ddfd24b4d2116a65d4482a89d83a93ac9cc1b1
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2026-09-16 12:47:42 +0300

    feat: Create password helpers.

---
+++ src/lib/password.ts
@@ -0,0 +1,33 @@
+import { randomBytes, scrypt as scryptCallback, timingSafeEqual } from "crypto";
+import { promisify } from "util";
+
+const scrypt = promisify(scryptCallback);
+
+export async function hashPassword(password: string): Promise<string> {
+  const salt = randomBytes(16).toString("hex");
+
+  const derivedKey = (await scrypt(password, salt, 64)) as Buffer;
+
+  return `${salt}:${derivedKey.toString("hex")}`;
+}
+
+export async function verifyPassword(
+  password: string,
+  storedHash: string,
+): Promise<boolean> {
+  const [salt, key] = storedHash.split(":");
+
+  if (!salt || !key) {
+    return false;
+  }
+
+  const derivedKey = (await scrypt(password, salt, 64)) as Buffer;
+
+  const storedKey = Buffer.from(key, "hex");
+
+  if (derivedKey.length !== storedKey.length) {
+    return false;
+  }
+
+  return timingSafeEqual(derivedKey, storedKey);
+}
